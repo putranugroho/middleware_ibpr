3639244d6c2d541b4048fe9c93f3382663e4d8dc
edicybereye <edi.cybereye@gmail.com>
2023-12-16 06:20:41 +0000

    commit fadly perubahan format tanggal trans

--- controller/gateway_bpr_new.js
+++ controller/gateway_bpr_new.js
@@ -2444,6 +2444,7 @@ const withdrawal = async (req, res) => {
                 }
             }
         } else if (trx_code == "1100") {
+            tgl_trans = moment().format("YYMMDDHHmmss")
             let jurnal_bpr
             if (keterangan == "acquirer") {
                 jurnal_bpr = acq_id
